ebf5730f9946e9d1e115c1ba35cdec67d3c84cff
Cissoko420 <59943539+Cissoko420@users.noreply.github.com>
2024-07-23 23:54:16 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className='bg-[url(/images/wallpaper.jpg)] overflow-y-auto bg-cover bg-center flex min-h-screen flex-col items-center justify-between font-[NothingSomething]'>
-      <Link href='/content'>Button</Link>
+      <Link
+        href='/content'
+        className='bg-blue-500 text-white px-6 py-3 text-lg rounded-md'
+      >
+        Button
+      </Link>
     </main>
   )
 }
